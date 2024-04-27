9975ae132e7c16377f023c827c7933db32267aac
Rishabh Gupta <rishabhguptaxdev@gmail.com>
2024-04-27 18:12:14 +0530

    created health route and controller and used necessary middlewares

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+//Import all routes
+const home = require("./routes/home");
+
 require("dotenv").config();
 
 const app = express();
 
-app.use(morgan("tiny"));
+// regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/health", (req, res) => {
-  console.log("Healthy");
-  res.send("Healthy");
-});
+// cookies and middlewares
+app.use(cookieParser());
+
+app.use(morgan("tiny"));
+app.use("/api/v1", home);
 
 module.exports = app;
